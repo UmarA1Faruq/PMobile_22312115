1e73f2d5c4c82e9d99effff3a193bba7254dfa15
umaralfaruq1424 <umaralfaruq1424@gmail.com>
2024-11-14 05:06:56 +0000

    Menambahkan halaman mahasiswa

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:myapp/app/controllers/auth_controller.dart';
+import 'package:myapp/app/modules/mahasiswa/views/mahasiswa_view.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -8,32 +9,132 @@ class HomeView extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
+    return DashboardAdmin();
+  }
+}
+
+class DashboardAdmin extends StatefulWidget {
+  const DashboardAdmin({super.key});
+
+  @override
+  State<DashboardAdmin> createState() => _DashboardAdminState();
+}
+
+class _DashboardAdminState extends State<DashboardAdmin> {
+  final cAuth = Get.find<AuthController>();
+  int _index = 0;
+  List<Map> _fragment = [
+    {'title': 'Dashboard', 'view': MahasiswaView()},
+    {'title': 'Data Mahasiswa', 'view': MahasiswaView()},
+    {'title': 'Data Dosen', 'view': MahasiswaView()},
+    {'title': 'Data Pegawai', 'view': MahasiswaView()},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
+      drawer: drawer(),
       appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 0, 121, 186),
+        titleSpacing: 0,
+        title: Text(_fragment[_index]['title']),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'HomeView is working',
-              style: TextStyle(fontSize: 20),
+    );
+  }
+
+  Widget drawer() {
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 0, 121, 186),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                cAunt.logout();
-              },
-              child: Text("Log Out"),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color.fromARGB(255, 105, 150, 34),
-                foregroundColor: Colors.white,
-              ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Icon(
+                  Icons.person,
+                  size: 80,
+                  color: Colors.white,
+                ),
+                Text(
+                  "Umar Al Faruq",
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                SizedBox(
+                  height: 2,
+                ),
+                Text(
+                  'Admin',
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          ListTile(
+            onTap: () {
+              setState(() => _index = 0);
+              Get.back();
+            },
+            leading: Icon(Icons.dashboard),
+            title: Text('Dashboard'),
+            trailing: Icon(Icons.navigate_next),
+            iconColor: Colors.blueAccent,
+            textColor: Colors.blueAccent,
+          ),
+          ListTile(
+            onTap: () {
+              setState(() => _index = 1);
+              Get.back();
+            },
+            leading: Icon(Icons.people),
+            title: Text('Data Mahasiswa'),
+            trailing: Icon(Icons.navigate_next),
+            iconColor: Colors.blueAccent,
+            textColor: Colors.blueAccent,
+          ),
+          ListTile(
+            onTap: () {
+              setState(() => _index = 2);
+              Get.back();
+            },
+            leading: Icon(Icons.people),
+            title: Text('Data Dosen'),
+            trailing: Icon(Icons.navigate_next),
+            iconColor: Colors.blueAccent,
+            textColor: Colors.blueAccent,
+          ),
+          ListTile(
+            onTap: () {
+              setState(() => _index = 3);
+              Get.back();
+            },
+            leading: Icon(Icons.people),
+            title: Text('Data Pegawai'),
+            trailing: Icon(Icons.navigate_next),
+            iconColor: Colors.blueAccent,
+            textColor: Colors.blueAccent,
+          ),
+          ListTile(
+            onTap: () {
+              Get.back();
+              cAuth.logout();
+            },
+            leading: Icon(Icons.logout),
+            title: Text('Logout'),
+            trailing: Icon(Icons.navigate_next),
+            iconColor: Colors.blueAccent,
+            textColor: Colors.blueAccent,
+          ),
+        ],
       ),
     );
   }
